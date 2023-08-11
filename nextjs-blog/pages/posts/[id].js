adb542e32a6a02a6cf2b5f5882b09235154dc7a4
lsh-sekiroxx <1611699255@qq.com>
2023-08-11 19:36:14 +0800

    Init

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false, 
+  };
+}
+
+// 静态生成只运行在服务端 可以在getStaticProps 插入数据库查询获取数据
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    }
+  };
+}
+
+export default function Post({ postData }) {
+    return (
+    <Layout>
+      <Head>
+        <title>{ postData.title }</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+    );
+};
